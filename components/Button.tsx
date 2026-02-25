654133ff60e2234092d69d6fc4e2c5cde772d5e4
Gizy Michel <batblover@yahoo.com>
2026-02-25 16:48:21 -0500

    initialized git repository

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,20 +1,21 @@
+import Image from 'next/image';
 import React from 'react'
 
-const Button = ({ type, title, icon, variant, full }) => {
-  return (
-    <button
-    type={type}
-    className={`btn ${variant} ${full ? "btn--full" : ""}`}
-    >
-      {icon && (
-        <img src={icon} 
-        alt={title} 
-        className="btn__icon"
-        />
-      )}
-      <span className="btn__label">{title}</span>
-    </button>
-  );
-};
+type ButtonProps = {
+    type: "button" | "submit";
+    title: string;
+    icon?: string;
+    variant: string;
+    full?: boolean
+}
 
-export default Button
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+    return (
+        <button type={type} className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>
+            {icon && <Image src={icon} alt={title} width={24} height={24} />}
+            <label className='bold-16 whitespace-nowrap cursor-pointer'>{title}</label>
+        </button>
+    )
+}
+
+export default Button;

a9d8b06d2b00e02d5fd263bf12c43b787beeff7e
Gizy Michel <batblover@yahoo.com>
2026-02-26 17:00:33 -0500

    initialized git repository

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,85 +1,85 @@
-import Image from "next/image";
-import React from "react";
-import Button from "./Button";
+import Image from 'next/image'
+import React from 'react'
+import Button from './Button'
 
 const Hero = () => {
-  return (
-    <section className="hero">
-      <div className="hero__map" />
+    return (
+        <section className='max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row'>
+            <div className='hero-map' />
 
-      <div className="hero__content">
-        <Image
-          src="/camp.svg"
-          alt="camp"
-          width={50}
-          height={50}
-          className="hero__camp-icon"
-        />
-
-        <h1 className="hero__title">Triund Hill Camp Area</h1>
-
-        <p className="hero__description">
-          We want to be on each of your journeys seeking the satisfaction of
-          seeing the incorruptible beauty of nature. We can help you on an
-          adventure around the world in just one app
-        </p>
-
-        <div className="hero__reviews-row">
-          <div className="hero__stars">
-            {Array(5)
-              .fill(1)
-              .map((_, index) => (
+            <div className='relative z-20 flex flex-1 flex-col xl:w-1/2'>
                 <Image
-                  src="/star.svg"
-                  key={index}
-                  alt="star"
-                  width={24}
-                  height={24}
+                    src="/camp.svg"
+                    alt='camp'
+                    width={50}
+                    height={50}
+                    className='absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]'
                 />
-              ))}
-          </div>
 
-          <p className="hero__reviews-text">
-            198k <span className="hero__reviews-subtext">Excellet Reviews</span>
-          </p>
-        </div>
+                <h1 className='bold-52 lg:bold-88'>Triund Hill Camp Area</h1>
+                <p className='regular-16 mt-6 text-gray-30 xl:max-w-[520px]'>We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app</p>
 
-        <div className="hero__buttons">
-          <Button type="button" title="Download App" variant="btn_green" />
-          <Button
-            type="button"
-            title="How we work?"
-            icon="/play.svg"
-            variant="btn_white_text"
-          />
-        </div>
-      </div>
+                <div className='my-11 flex flex-wrap gap-5'>
+                    <div className='flex items-center gap-2'>
+                        {Array(5).fill(1).map((_, index) => (
+                            <Image
+                                src="/star.svg"
+                                key={index}
+                                alt='star'
+                                width={24}
+                                height={24}
+                            />
+                        ))}
+                    </div>
 
-      <div className="hero__card-wrap">
-        <div className="hero__card">
-          <div className="hero__card-block">
-            <div className="hero__card-top">
-              <p className="hero__card-label">Location</p>
-              <Image src="/close.svg" alt="close" width={24} height={24} />
+                    <p className='bold-16 lg:bold-20 text-blue-70'>
+                        198k
+                        <span className='regular-16 lg:regular-20 ml-1'>Excellet Reviews</span>
+                    </p>
+                </div>
+
+
+                <div className='flex flex-col w-full gap-3 sm:flex-row'>
+                    <Button
+                        type='button'
+                        title='Download App'
+                        variant='btn_green'
+                    />
+                    <Button
+                        type='button'
+                        title='How we work?'
+                        icon='/play.svg'
+                        variant='btn_white_text'
+
+                    />
+                </div>
             </div>
-            <p className="hero__card-value">Triund Hill</p>
-          </div>
 
-          <div className="hero__card-grid">
-            <div className="hero__card-col">
-              <p className="hero__card-label">Distance</p>
-              <p className="hero__card-value">18 Km</p>
-            </div>
 
-            <div className="hero__card-col">
-              <p className="hero__card-label">Elevation</p>
-              <p className="hero__card-value">2.850 Km</p>
+            <div className='relative flex flex-1 items-start'>
+                <div className='relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8'>
+                    <div className='flex flex-col'>
+                        <div className='flexBetween'>
+                            <p className='regular-16 text-gray-20'>Location</p>
+                            <Image src="/close.svg" alt='close' width={24} height={24} />
+                        </div>
+                        <p className='bold-20 text-white'>Triund Hill</p>
+                    </div>
+
+                    <div className='flexBetween'>
+                        <div className='flex flex-col'>
+                            <p className='regular-16 block text-gray-20'>Distance</p>
+                            <p className='bold-20 text-white'>18 Km</p>
+                        </div>
+                        <div className='flex flex-col'>
+                            <p className='regular-16 block text-gray-20'>Elevation</p>
+                            <p className='bold-20 text-white'>2.850 Km</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
-};
+        </section>
+    )
+}
 
 export default Hero;

1e33c929c46962a69ad99c8cf76c838ac47473fc
Gizy Michel <batblover@yahoo.com>
2026-04-13 15:19:31 -0400

    initialized git repository

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,42 +1,53 @@
-import { NAV_LINKS } from '@/constants'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Button from './Button'
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Button from "./Button";
 
 const Navbar = () => {
-    return (
-        <nav className='flexBetween max-container padding-container relative z-30 py-5'>
-            <Link href="/">
-                <Image src="/hilink-logo.svg" alt='logo' width={74} height={29} />
+  return (
+    <nav className="w-full max-w-[1440px] mx-auto px-6 lg:px-20 py-5 flex items-center justify-between relative z-30">
+      <Link href="/" className="shrink-0">
+        <Image
+          src="/hilink-logo.svg"
+          alt="logo"
+          width={74}
+          height={29}
+          priority
+        />
+      </Link>
+
+      <ul className="hidden lg:flex items-center gap-12 absolute left-1/2 -translate-x-1/2">
+        {NAV_LINKS.map((link) => (
+          <li key={link.key}>
+            <Link
+              href={link.href}
+              className="text-[16px] font-normal text-[#e3e3e3] pb-1.5 transition-all hover:font-bold"
+            >
+              {link.label}
             </Link>
+          </li>
+        ))}
+      </ul>
 
-            <ul className='hidden h-full gap-12 lg:flex'>
-                {NAV_LINKS.map((link) => (
-                    <Link href={link.href} key={link.key} className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'>
-                        {link.label}
-                    </Link>
-                ))}
-            </ul>
+      <div className="hidden lg:flex items-center justify-center shrink-0">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
 
-            <div className='lg:flexCenter hidden'>
-                <Button
-                    type="button"
-                    title="Login"
-                    icon="/user.svg"
-                    variant="btn_dark_green"
-                />
-            </div>
-
-            <Image
-                src="menu.svg"
-                alt='menu'
-                width={32}
-                height={32}
-                className='inline-block cursor-pointer lg:hidden'
-            />
-        </nav>
-    )
-}
+      <button
+        type="button"
+        className="lg:hidden inline-flex items-center justify-center bg-transparent border-none p-0 cursor-pointer shrink-0"
+        aria-label="Open menu"
+      >
+        <Image src="/menu.svg" alt="menu" width={32} height={32} />
+      </button>
+    </nav>
+  );
+};
 
 export default Navbar;
